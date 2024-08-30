97a1083a077747f838c051ea062a71d453598d72
Isra5000 <israel.rod5000@gmail.com>
2024-08-30 17:46:07 -0600

    Elaboración de mapa 1

---
+++ Prueba 2.1.R
@@ -0,0 +1,7 @@
+# Mapa 1 ------------------------------------------------------------------
+
+# comentarios
+# cargar paquetes, empezando del que se utilizará menos al que se utilizara más #
+library(sf)
+library(dplyr)
+
